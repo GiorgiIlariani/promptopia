31027dfc3397feba905a718931b81e5b4368ad32
GiorgiIlariani <lmaisuradze3333@gmail.com>
2023-09-12 13:28:09 +0400

    uploades project

---
+++ app/layout.jsx
@@ -0,0 +1,28 @@
+import "@/styles/globals.css";
+import Nav from "@/components/nav";
+import Provider from "@/components/provider";
+import { ToastContainer } from "react-toastify";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body>
+      <Provider>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
